6f3a48ed2ca89a7e14c2c2a2362f991e13892d09
user48 <user48@gntp-48.gntp.local>
2018-04-27 14:45:47 +0900

    키보드로 입력받기 추가

--- HelloGit/src/com/Hellowgit.java
+++ HelloGit/src/com/Hellowgit.java
@@ -1,9 +1,15 @@
 package com;
 
+import java.util.Scanner;
+
 public class Hellowgit {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO");
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String name=sc.next();
 
 	}
 
