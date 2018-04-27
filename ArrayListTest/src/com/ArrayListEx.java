5ebbfe98432f0bb19892695bb85162ffbde96343
user48 <user48@gntp-48.gntp.local>
2018-04-27 16:06:10 +0900

    머라고 해야할까

---
+++ ArrayListTest/src/com/ArrayListEx.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		ArrayList<String> a=new ArrayList<String>();
+		
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<4;i++) {
+			System.out.println("이름을 입력하세요");
+			String s=sc.next();
+			a.add(s);
+		}
+		for(int i=0;i<a.size();i++) {
+			String name=a.get(i);
+			System.out.println(name+"");
+			
+		}
+		int longestIndex=i;
+		for(a.get(index))
+			
+		
+	
+		
+		
+
+	}
+
+}
